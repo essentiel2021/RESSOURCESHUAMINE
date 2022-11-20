da97c785725717e8186103ee3d6ab8b3db64743f
jatachi <achijathniel@gmail.com>
2022-11-20 22:44:56 +0000

    recherche par nom prenom matricule realiser

--- app/Http/Livewire/BlackList.php
+++ app/Http/Livewire/BlackList.php
@@ -6,36 +6,20 @@ use App\Models\Employe;
 use Livewire\Component;
 use Livewire\WithPagination;
 
-class Employes extends Component
+class BlackList extends Component
 {
     use WithPagination;
     protected $paginationTheme = "bootstrap";
 
     public $search = "";
-    
-    public $currentPage = PAGELISTEMPLOYE;
     public function render()
     {
         $employeQuery = Employe::query();
-        
         $employeQuery->where("nom", "LIKE",  "%". $this->search ."%")
                     ->orWhere("matricule","LIKE",  "%". $this->search ."%")
                     ->orWhere("prenom","LIKE",  "%". $this->search ."%");
 
         $data = ["employes" => $employeQuery->latest()->paginate(5)];
-        return view('livewire.employes.index',$data)->extends("layouts.master")->section("contenu");
+        return view('livewire.blackList.list',$data);
     }
-
-    public function goToAddEmployee(){
-        $this->currentPage = PAGECREATEFORMTEMPLOYE;
-    }
-
-    public function goToListEmployee(){
-        $this->currentPage = PAGELISTEMPLOYE;
-    }
-
-    public function goToEditEmployee(){
-        $this->currentPage = PAGEEDITFORMTEMPLOYE;
-    }
-   
 }
---
+++ resources/views/livewire/blackList/list.blade.php
@@ -0,0 +1,65 @@
+<div class="row p-4 pt-5">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header bg-primary">
+                <h3 class="card-title flex-grow-1"><i class="fas fa-users fa-2x"></i>Liste des employés</h3>
+                <div class="card-tools d-flex align-items-center">
+                    <a class="btn btn-link text-white mr-4 d-block"><i class="fas fa-user-plus"></i>Nouveau employé</a>
+                    <div class="input-group input-group-md" style="width: 250px;">
+                        <input type="text" name="table_search" wire:model.debounce.250ms="search" class="form-control float-right" placeholder="Recherche">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-default">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body table-responsive p-0 table-striped" >
+                <table class="table table-head-fixed text-nowrap">
+                    <thead>
+                        <tr>
+                            <th style="width:5%;"></th>
+                            <th style="width:20%;">Matricule</th>
+                            <th style="width:20%;">Nom</th>
+                            <th style="width:20%;">Prenom</th>
+                            <th style="width:20%;"class="text-center">Situation matrimoniale</th>
+                            <th style="width:10%;"class="text-center">Commune</th>
+                            <th style="width:5%;"class="text-center">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($employes as $employe)
+                            <tr>
+                                <td><img src="{{asset('images/imageplaceholder.png')}}" style="width:60px;height:60px;"></td>
+                                <td>{{$employe->matricule}}</td>
+                                <td>{{$employe->nom}}</td>
+                                <td>{{$employe->prenom}}</td>
+                                <td class="text-center">{{$employe->situation->libelle }}</td>
+                                <td class="text-center">{{$employe->commune->libelle }}</td>
+                                <td class="text-center">
+                                    <button class="btn btn-link"> <i class="far fa-edit"></i></button>
+                                    <button class="btn btn-link"><i class="far fa-trash-alt"></i></button>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="5">
+                                    <div class="alert alert-danger">
+                                        <h5><i class="icon fas fa-ban"></i> Information!</h5>
+                                        Aucune donnée en Base.
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-footer">
+                <div class="float-right">
+                    {{ $employes->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
